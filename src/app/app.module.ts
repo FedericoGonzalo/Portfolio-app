d7bd2c7ca02bb52aaae2a2312c8e5a93f6dcb126
Federico Suárez Martín <f.suarezmartin@gmail.com>
2022-03-10 19:07:44 -0300

    Pruebas,

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { AcercaComponent } from './componentes/acerca/acerca.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { ProyectoComponent } from './componentes/proyecto/proyecto.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { HttpClientModule } from  '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module.ts.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    AcercaComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    SkillsComponent,
+   
+    ProyectoComponent,
+        LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
