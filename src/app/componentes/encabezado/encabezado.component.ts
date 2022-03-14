c57cf81b422356531b61c00efd42c53d89fa54e5
Federico Suárez Martín <f.suarezmartin@gmail.com>
2022-03-14 02:21:08 -0300

    mas avances, ahora ya hay un binding json

--- src/app/componentes/encabezado/encabezado.component.ts
+++ src/app/componentes/encabezado/encabezado.component.ts
@@ -7,11 +7,14 @@ import { PortfolioService } from 'src/app/servicios/portfolio.service';
   styleUrls: ['./encabezado.component.css']
 })
 export class EncabezadoComponent implements OnInit {
-
+   miPortfolio:any;
   constructor(private datosPorfolio:PortfolioService) { }
 
   ngOnInit(): void {
-    this.datosPorfolio.obtenerDatos();
+    this.datosPorfolio.obtenerDatos().subscribe(data=>{
+      console.log(data);
+      this.miPortfolio=data;
+    });
   }
 
 }
