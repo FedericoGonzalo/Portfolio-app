4794db13d84a281e6b5fda0c2154ec1a28e1af1b
Federico Suárez Martín <f.suarezmartin@gmail.com>
2022-05-17 15:30:17 -0300

    Proycto y Skill potable

--- src/app/componentes/proyecto/proyecto.component.ts
+++ src/app/componentes/proyecto/proyecto.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
 @Component({
   selector: 'app-proyecto',
   templateUrl: './proyecto.component.html',
   styleUrls: ['./proyecto.component.css']
 })
 export class ProyectoComponent implements OnInit {
-
-  constructor() { }
+proyectoList:any;
+  constructor(private datosPortFolio:PortfolioService) { }
 
   ngOnInit(): void {
+  this.datosPortFolio.obtenerDatos().subscribe(data=>{console.log(data),
+  this.proyectoList=data.proyectos;})
+  
   }
 
 }
