32fff727c616a6151452046caa1f3bc6fa573740
Federico Suárez Martín <f.suarezmartin@gmail.com>
2022-05-20 23:02:45 -0300

    agregue supestos enpoints

--- src/app/servicios/portfolio.service.ts
+++ src/app/servicios/portfolio.service.ts
@@ -22,7 +22,44 @@ export class PortfolioService {
   }
       */
 
-  
+  editNombre(nombre:any):Observable<any>{
+    return this.http.put<any>('https://argprogbackend.herokuapp.com/api/personas/persona/editar/1/nombre',nombre);  
+   }
+
+
+   editApellido(data:any):Observable<any>{
+    return this.http.put<any>('https://argprogbackend.herokuapp.com/api/personas/persona/editar/1/apellido',data);  
+   }
+   editNacimiento(data:any):Observable<any>{
+    return this.http.put<any>('https://argprogbackend.herokuapp.com/api/personas/persona/editar/1/nac',data);  
+   }
+
+   editFotoUrl(data:any):Observable<any>{
+    return this.http.put<any>('https://argprogbackend.herokuapp.com/api/personas/persona/editar/1/foto',data);  
+   }
+
+   editImgHeader(data:any):Observable<any>{
+    return this.http.put<any>('https://argprogbackend.herokuapp.com/api/personas/persona/editar/1/img',data);  
+   }
+
+   editAcercaTexto(data:any):Observable<any>{
+    return this.http.put<any>('https://argprogbackend.herokuapp.com/api/personas/persona/editar/1/acercaTXT',data);  
+   }
+
+   editResidencia(data:any):Observable<any>{
+    return this.http.put<any>('https://argprogbackend.herokuapp.com/api/personas/persona/editar/1/residencia',data);  
+   }
+
+   editUrlResidencia(data:any):Observable<any>{
+    return this.http.put<any>('https://argprogbackend.herokuapp.com/api/personas/persona/editar/1/urlResidencia',data);  
+   }
+   
+   /*  acerca de  */
+
+   editAcercaPersona(data:any):Observable<any>{
+    return this.http.post<any>('https://argprogbackend.herokuapp.com/api/personas/persona/ver/1/edit/AcercaPersona',data);  
+   }
+   
 
 
 
